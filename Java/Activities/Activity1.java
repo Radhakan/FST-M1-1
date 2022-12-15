cf54515c84f49535fca870c1f0eef54d32fd6b3c
Radhakan <112602694+Radhakan@users.noreply.github.com>
2022-12-15 13:46:31 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,15 @@
+package activities;
+
+public class Activity1 {
+
+       public static void main(String[] args){
+        Car Venue = new Car();
+
+           Venue.make = 2014;
+           Venue.color = "Black";
+           Venue.transmission = "Manual";
+           Venue.displayCharacteristics();
+           Venue.accelerate();
+           Venue.brake();
+    }
+}
